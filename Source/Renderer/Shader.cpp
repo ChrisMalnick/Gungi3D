63d1e48afcbd3d518f597caaeb7a4c3cc3c3a9fd
Chris Malnick <chrismalnick@yahoo.com>
2019-01-07 16:04:06 -0500

    Update Shader.cpp

--- Source/Renderer/Shader.cpp
+++ Source/Renderer/Shader.cpp
@@ -1,19 +1,19 @@
 /* ============================================================================
  * Project:	Gungi3D
- *
+ * 
  * File:	Shader.cpp
- *
+ * 
  * Summary:	Imports and compiles shader modules at runtime that are necessary
- *			to process and render all graphical components
- *
+ *		to process and render all graphical components
+ * 
  * Origin:	N/A
- *
+ * 
  * Legal:	Unregistered Copyright (C) 2018 Chris Malnick - All Rights Reserved
- *			Unauthorized duplication, reproduction, modification, and/or
- *			distribution is strictly prohibited
- *			All materials, including, but not limited to, code, resources
- *			(models, textures, etc.), documents, etc. are deliberately
- *			unlicensed
+ *		Unauthorized duplication, reproduction, modification, and/or
+ *		distribution is strictly prohibited
+ *		All materials, including, but not limited to, code, resources
+ *		(models, textures, etc.), documents, etc. are deliberately
+ *		unlicensed
  * ============================================================================
  */
 
