3fdd8220a4cd39555a424ca5811d865cde2bf6d3
Chris Malnick <chrismalnick@yahoo.com>
2019-01-07 16:20:48 -0500

    Update Depthmap_2D.fs

--- Resources/Shaders/Depthmap/2D/Depthmap_2D.fs
+++ Resources/Shaders/Depthmap/2D/Depthmap_2D.fs
@@ -1,19 +1,19 @@
 /* ============================================================================
  * Project:	Gungi3D
- *
+ * 
  * File:	Depthmap_2D.fs
- *
+ * 
  * Summary:	Outputs the depth value of a fragment from a light's perspective
- *			relative to a single direction in clip space
- *
+ *		relative to a single direction in clip space
+ * 
  * Origin:	N/A
- *
+ * 
  * Legal:	Unregistered Copyright (C) 2018 Chris Malnick - All Rights Reserved
- *			Unauthorized duplication, reproduction, modification, and/or
- *			distribution is strictly prohibited
- *			All materials, including, but not limited to, code, resources
- *			(models, textures, etc.), documents, etc. are deliberately
- *			unlicensed
+ *		Unauthorized duplication, reproduction, modification, and/or
+ *		distribution is strictly prohibited
+ *		All materials, including, but not limited to, code, resources
+ *		(models, textures, etc.), documents, etc. are deliberately
+ *		unlicensed
  * ============================================================================
  */
 
