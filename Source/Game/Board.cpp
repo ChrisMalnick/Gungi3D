d24e43e9645cda8a3050f871e6ec0916d2dff4d4
Chris Malnick <chrismalnick@yahoo.com>
2019-01-07 15:58:30 -0500

    Update Board.cpp

--- Source/Game/Board.cpp
+++ Source/Game/Board.cpp
@@ -1,19 +1,19 @@
 /* ============================================================================
  * Project:	Gungi3D
- *
+ * 
  * File:	Board.cpp
- *
+ * 
  * Summary:	Maintains a logical representation of the game board and handles
- *			all game logic
- *
+ *		all game logic
+ * 
  * Origin:	N/A
- *
+ * 
  * Legal:	Unregistered Copyright (C) 2018 Chris Malnick - All Rights Reserved
- *			Unauthorized duplication, reproduction, modification, and/or
- *			distribution is strictly prohibited
- *			All materials, including, but not limited to, code, resources
- *			(models, textures, etc.), documents, etc. are deliberately
- *			unlicensed
+ *		Unauthorized duplication, reproduction, modification, and/or
+ *		distribution is strictly prohibited
+ *		All materials, including, but not limited to, code, resources
+ *		(models, textures, etc.), documents, etc. are deliberately
+ *		unlicensed
  * ============================================================================
  */
 
