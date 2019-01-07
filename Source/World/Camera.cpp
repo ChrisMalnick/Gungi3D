6ad78978a42756fdcd720c30f9a2b7115efad29c
Chris Malnick <chrismalnick@yahoo.com>
2019-01-07 16:05:07 -0500

    Update Camera.cpp

--- Source/World/Camera.cpp
+++ Source/World/Camera.cpp
@@ -1,19 +1,19 @@
 /* ============================================================================
  * Project:	Gungi3D
- *
+ * 
  * File:	Camera.cpp
- *
+ * 
  * Summary:	Manages the various properties that determine the position and
- *			orientation of the viewing frustum relative to the scene
- *
+ *		orientation of the viewing frustum relative to the scene
+ * 
  * Origin:	N/A
- *
+ * 
  * Legal:	Unregistered Copyright (C) 2018 Chris Malnick - All Rights Reserved
- *			Unauthorized duplication, reproduction, modification, and/or
- *			distribution is strictly prohibited
- *			All materials, including, but not limited to, code, resources
- *			(models, textures, etc.), documents, etc. are deliberately
- *			unlicensed
+ *		Unauthorized duplication, reproduction, modification, and/or
+ *		distribution is strictly prohibited
+ *		All materials, including, but not limited to, code, resources
+ *		(models, textures, etc.), documents, etc. are deliberately
+ *		unlicensed
  * ============================================================================
  */
 
