bbdaa737b5ec3c04f6053189630dfbde68465acf
Chris Malnick <chrismalnick@yahoo.com>
2019-01-07 16:16:31 -0500

    Update RedirectStandardIO.h

--- Headers/Windows/RedirectStandardIO.h
+++ Headers/Windows/RedirectStandardIO.h
@@ -1,19 +1,19 @@
 /* ============================================================================
  * Project:	Gungi3D
- *
+ * 
  * File:	RedirectStandardIO.h
- *
+ * 
  * Summary:	Redirects standard input and output to the parent console and
- *			parses and verifies command line arguments
- *
+ *		parses and verifies command line arguments
+ * 
  * Origin:	N/A
- *
+ * 
  * Legal:	Unregistered Copyright (C) 2018 Chris Malnick - All Rights Reserved
- *			Unauthorized duplication, reproduction, modification, and/or
- *			distribution is strictly prohibited
- *			All materials, including, but not limited to, code, resources
- *			(models, textures, etc.), documents, etc. are deliberately
- *			unlicensed
+ *		Unauthorized duplication, reproduction, modification, and/or
+ *		distribution is strictly prohibited
+ *		All materials, including, but not limited to, code, resources
+ *		(models, textures, etc.), documents, etc. are deliberately
+ *		unlicensed
  * ============================================================================
  */
 
