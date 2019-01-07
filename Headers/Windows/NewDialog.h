66c17783c54a06662e0619cd95752b976914b79c
Chris Malnick <chrismalnick@yahoo.com>
2019-01-07 16:16:13 -0500

    Update NewDialog.h

--- Headers/Windows/NewDialog.h
+++ Headers/Windows/NewDialog.h
@@ -1,19 +1,19 @@
 /* ============================================================================
  * Project:	Gungi3D
- *
+ * 
  * File:	NewDialog.h
- *
+ * 
  * Summary:	Creates, handles, and retrieves data from a custom Windows based
- *			dialog
- *
+ *		dialog
+ * 
  * Origin:	N/A
- *
+ * 
  * Legal:	Unregistered Copyright (C) 2018 Chris Malnick - All Rights Reserved
- *			Unauthorized duplication, reproduction, modification, and/or
- *			distribution is strictly prohibited
- *			All materials, including, but not limited to, code, resources
- *			(models, textures, etc.), documents, etc. are deliberately
- *			unlicensed
+ *		Unauthorized duplication, reproduction, modification, and/or
+ *		distribution is strictly prohibited
+ *		All materials, including, but not limited to, code, resources
+ *		(models, textures, etc.), documents, etc. are deliberately
+ *		unlicensed
  * ============================================================================
  */
 
